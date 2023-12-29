46aa71b25a4d99c3960503d367c6c2ef7e2c5402
Idriz Altes <99037664+LetRookie@users.noreply.github.com>
2023-12-29 19:40:37 +0200

    Videos routes and video controler created

--- routes/videos.js
+++ routes/videos.js
@@ -1,7 +1,18 @@
 import express from "express";
-import { test } from "../controlers/video.js";
+import { addVideo, addView, deleteVideo, getVideo, random, sub, trend, updateVideo } from "../controlers/video.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
+
+//create a video
+router.post("/", verifyToken, addVideo)
+router.put("/:id", verifyToken, updateVideo)
+router.delete("/", verifyToken, deleteVideo)
+router.get("/find/:id", getVideo)
+router.put("/view/:id", addView)
+router.get("/trend", trend)
+router.get("/random", random)
+router.get("/sub", verifyToken, sub)
 
 
 export default router;
